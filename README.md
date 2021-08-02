314a0ed238d1006f958c39ed1364b2fd66315de0
Phil Cockfield <phil@cockfield.net>
2021-08-02 19:37:20 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Vercel API Example: File Upload
+
+Related to support conversation with Vercel.
+
+token read from env variable: `process.env.VERCEL_TEST_TOKEN`
+
+```
+yarn install
+yarn start
+```
+
+Produces error:
+
+```
+res.status 400
+res.json {
+  error: {
+    code: 'sha1sum_mismatch',
+    message: "SHA1 sum of the file (7821587476f0529a1c4c46859139fe212fe33024) doesn't match the provided SHA1 sum (621d61ebade0cb43421d3ad40e555c5055a75991)"
+  }
+}
+```
+
+Looking for how to appropriate generate the SHA1 hash and use it to post a file to the `https://api.vercel.com/v11/now/files` endpoint.
+
+## Refs
+
+- https://vercel.com/docs/api#endpoints/deployments/upload-deployment-files
+- https://vercel.com/docs/integrations#webhooks/securing-webhooks
---
+++ index.ts
@@ -0,0 +1,41 @@
+import crypto from 'crypto';
+import fs from 'fs-extra';
+import { resolve } from 'path';
+import { http } from '@platform/http';
+
+const token = process.env.VERCEL_TEST_TOKEN ?? '';
+
+function toSha1(file: Buffer) {
+  return crypto.createHmac('sha1', token).update(file).digest('hex');
+}
+
+/**
+ * See:
+ *    https://vercel.com/docs/api#endpoints/deployments/upload-deployment-files
+ *
+ * Suggestion from Vercel suuport (example) of generating a
+ * SHA1 digest hash for file upload:
+ *
+ *    https://vercel.com/docs/integrations#webhooks/securing-webhooks
+ *
+ */
+async function sample() {
+  const path = resolve('static/hand.png');
+  const file = await fs.readFile(path);
+  const hash = toSha1(file);
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    'x-vercel-digest': hash,
+    'Content-Length': file.byteLength.toString(),
+    'Content-Type': 'application/octet-stream',
+  };
+
+  const url = 'https://api.vercel.com/v11/now/files';
+  const res = await http.post(url, file, { headers });
+
+  console.log('res.status', res.status);
+  console.log('res.json', res.json);
+}
+
+sample();
